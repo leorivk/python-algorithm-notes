689010a77afe9d9470555943b00e403ed68ca4d1
ivk <leorivk@gmail.com>
2024-03-22 16:38:39 +0900

    Update selection_sort.py

--- Sorting/selection_sort.py
+++ Sorting/selection_sort.py
@@ -1,10 +1,16 @@
 data = [1, 4, 3, 2, 7, 3]
 
-for i in range(len(data)):
-    min_index = i
-    for j in range(i+1, len(data)):
-        if data[j] < data[min_index]:
-            min_index = j
-    data[min_index], data[i] = data[i], data[min_index]
-    
-print(data)
+'''
+단순 선택 정렬 알고리즘의 원솟값을 비교하는 횟수 : n(n-1)/2 번
+
+서로 이웃하지 않은 떨어져 있는 원소를 교환하므로 안정적이지 않다.
+'''
+
+def selection_sort(data):
+    n = len(data)
+    for i in range(n-1): # 정렬할 부분 중 가장 작은 원소의 인덱스
+        for j in range(i+1, n):
+            if data[j] < data[min]:
+                min = j
+        data[i], data[min] = data[min], data[i]
+        
