0062ad9839bfb30b0f7fbc0f36703884e0feef16
ivk <leorivk@gmail.com>
2024-03-22 23:19:20 +0900

    Create hashing.py

---
+++ Search/hashing.py
@@ -0,0 +1,63 @@
+import hashlib
+
+class Node:
+    
+    def __init__(self, key, value, next):
+        self.key = key
+        self.value = value
+        self.next = next
+ 
+class ChainedHash:
+    
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.table = [None] * self.capacity
+    
+    def hash_value(self, key):
+        
+        if isinstance(key, int):
+            return key % self.capacity
+        
+        # key 값이 int 형이 아닌 경우
+        return (int(hashlib.sha256(str(key).encode()).hexdigest(), 16) % self.capacity)
+
+    def search(self, key):
+        hash = self.hash_value(key)
+        p = self.table[hash]
+
+        while p is not None:
+            if p.key == key:
+                return p.value
+            p = p.next
+        
+        return None
+    
+    def add(self, key, value):
+        hash = self.hash_value(key)
+        p = self.table[hash]
+        
+        while p is not None:
+            if p.key == key:
+                return False
+            p = p.next
+        
+        temp = Node(key, value, self.table[hash])
+        self.table[hash] = temp
+        return True
+    
+    def remove(self, key):
+        hash = self.hash_value(key)
+        p = self.table[hash]
+        pp = None
+        
+        while p is not None:
+            if p.key == key:
+                if pp is None:
+                    self.table[hash] = p.next
+                else:
+                    pp.next = p.next
+                return True
+            pp = p
+            p = p.next 
+        return False
+    
